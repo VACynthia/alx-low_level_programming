3efc5dd7207877320f4d3d452e16cacb625a6b2a
VACynthia <cynthiavakenya@gmail.com>
2023-03-22 18:50:43 -0700

    Absolute value of integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,13 +4,14 @@
  * _abs - function that computes the absolute value of an integer
  *
  * @a: parameter to be checked
- * Return:always a
+ *
+ * Return: always a
  */
 
 int _abs(int a)
 {
 	if (a < 0)
-		a = (a);
+		a = -(a);
 	else if (a >= 0)
 		a = a;
 	return (a);
