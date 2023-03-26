ed69d56ed6b0c2da897fd8f6191e0c93a8c0592a
VACynthia <cynthiavakenya@gmail.com>
2023-03-25 17:52:14 -0700

    Fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int a, b, c, d, e;
+	int n = 50;
+
+	a = 1;
+	b = 2;
+
+	c = a + b;
+	d = c + b;
+	e = d + c;
+
+	printf("%d, \n")
+
+                return (0);
+}
