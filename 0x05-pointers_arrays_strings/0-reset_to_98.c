2efec7bbce451ce4672252a8a3586bf94d23a780
VACynthia <cynthiavakenya@gmail.com>
2023-03-29 18:55:32 -0700

    Dereferencing

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,8 @@
 #include "main.h"
 
 /**
- * main - the main function
- *
  * reset_to_98 - function that changes the value of n to 98, dereferencing
- *
  * @n: the value to be replaced
- *
  * Return: always 0
  */
 
