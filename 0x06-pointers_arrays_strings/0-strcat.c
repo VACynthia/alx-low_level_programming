294abb8c7e64b98775c1b7e33bc486b99c31fea6
VACynthia <cynthiavakenya@gmail.com>
2023-03-30 15:37:18 -0700

    Concatenate is to append

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - function that concatenates or appends two strings
+ * @dest: destination string
+ * @src: source string
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int destlength = 0;
+	int srclength = 0;
+	int i;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlength++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclength++;
+	for (i = 0 ; i <= srclength ; i++)
+		dest[destlength + i] = src[i];
+	return (dest);
+}
