fbec751d0acddb7d59afbcaaaffa0d889e9ada32
VACynthia <cynthiavakenya@gmail.com>
2023-04-08 12:33:39 -0700

    Sizeof variables on 32 bit & 64 bit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,16 +8,19 @@
 int main(void)
 {
 	char;
-	int;
-	long int;
-	long long int;
-	float;
 
-	printf("Size of a char: %d\n", sizeof(char));
-	printf("Size of an int: %d\n", sizeof(int));
-	printf("Size of a long int: %lu\n", sizeof(long int));
-	printf("Size of a long long int: %lu\n", sizeof(long long int));
-	printf("Size of a float: %d\n", sizeof(float));
+	int;
+
+	long int;
+
+	long long int;
+
+	float;
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", sizeof(float));
 
 	return (0);
 }
