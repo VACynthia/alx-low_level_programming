0f060cc9a30f0014a9fc52e1403db1d3c8c9f04f
VACynthia <cynthiavakenya@gmail.com>
2023-04-08 11:10:12 -0700

    Sizeof my variable types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ * Return: 0
+ */
+
+int main(void)
+{
+	char;
+	int;
+	long int;
+	long long int;
+	float;
+
+	printf("Size of a char: %d\n", sizeof(char));
+	printf("Size of an int: %d\n", sizeof(int));
+	printf("Size of a long int: %lu\n", sizeof(long int));
+	printf("Size of a long long int: %lu\n", sizeof(long long int));
+	printf("Size of a float: %d\n", sizeof(float));
+
+	return (0);
+}
