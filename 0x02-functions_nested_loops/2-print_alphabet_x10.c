b632028f9c3a6dd5bfd653fb1ac1ef47f778ada3
VACynthia <cynthiavakenya@gmail.com>
2023-04-11 02:57:18 -0700

    Lowercase alphabet *10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,13 +8,14 @@
 
 void print_alphabet_x10(void)
 {
-	int i;
-	char j;
+	char m;
+	int n;
 
-	for (i = 1 ; i <= 10 ; i++)
+	for (n = 1 ; n <= 10 ; n++)
 	{
-		for (j = 'a' ; j <= 'z' ; j++)
-			_putchar(j);
-		_putchar('\n');
+	for (m = 'a' ; m <= 'z' ; m++)
+
+		_putchar(m);
+	_putchar('\n');
 	}
 }
