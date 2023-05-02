227487497efddfa27e64d4619b4ba602d8dfe033
VACynthia <cynthiavakenya@gmail.com>
2023-05-02 13:02:56 -0700

    Duplicated string

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,30 +2,36 @@
 
 /**
  * _strdup - function returns pointer to duplicated string
- *
+ * @str: original string
  * Return: pointer on success
  * and NULL if memory is insufficient
  */
 
 char *_strdup(char *str)
 {
+	char *cpy;
+	int n;
+	int len = 0;
+
 	if (str == NULL)
 	{
 		return (NULL);
-	}/* length of string */
-	size_t len = strlen(str);
+	}
+	/* length of string */
+	while (str[len] != '\0')
+		len++;
 
 	/* allocate memory for the copy */
-	char * cpy = malloc((len + 1) * sizeof(char));
+	cpy = (char *)malloc((sizeof(char) * len) + 1);
 	if (cpy == NULL)
 	{
 		return (NULL);
 	}
 
 	/* copy string and null terminator */
-	strcpy(cpy, str);
+	for (n = 0 ; n < len ; n++)
+		cpy[n] = str[n];
 	cpy[len] = '\0';
 
 	return (cpy);
 }
-
