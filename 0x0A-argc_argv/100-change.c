0ebcb140d9672b8854ce6eff6bb167997f7adc5d
VACynthia <cynthiavakenya@gmail.com>
2023-05-01 19:54:39 -0700

    Change in coins

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * main - program that works out change in coins
+ * @argc: argument count
+ * @argv: arguments
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	cents = atoi(argv[1]);
+	if (cents < 0)
+	{
+		printf("\n");
+		return (0);
+	}
+	coins += cents / 25;
+	cents = cents % 25;
+	coins += cents / 10;
+	cents = cents % 10;
+	coins += cents / 5;
+	cents = cents % 5;
+	coins += cents / 2;
+	cents = cents % 2;
+
+	coins += cents;
+
+	printf("%d\n", coins);
+	return (0);
+}
